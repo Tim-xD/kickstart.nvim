644b40061c70df51dcc7bef6cb795a64412fa3a0
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-02-27 16:21:56 +0100

    feat: java dap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -69,6 +69,11 @@ return {
           -- get_nix_store '-jdt-language-server-' .. '/share/java/jdtls/config_linux',
         },
         root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+        init_options = {
+          bundles = {
+            vim.fn.glob('/home/tim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1),
+          },
+        },
       }
 
       require('jdtls').start_or_attach(config)
