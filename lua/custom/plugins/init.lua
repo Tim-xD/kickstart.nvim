6c50488b2371576c71871fea101bc310436b6dd7
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-04-23 10:20:13 +0200

    feat: lsp + cmp

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,22 +14,9 @@ return {
       vim.g.vimtex_view_method = 'general'
     end,
   },
-  -- {
-  --   'mfussenegger/nvim-jdtls',
-  --   ft = { 'java' },
-  -- },
   {
-    'mistweaverco/kulala.nvim',
-    keys = {
-      { '<leader>Rs', desc = 'Send request' },
-      { '<leader>Ra', desc = 'Send all requests' },
-      { '<leader>Rb', desc = 'Open scratchpad' },
-    },
-    ft = { 'http', 'rest' },
-    opts = {
-      -- your configuration comes here
-      global_keymaps = true,
-    },
+    'mfussenegger/nvim-jdtls',
+    ft = { 'java' },
   },
   {
     'danymat/neogen',
